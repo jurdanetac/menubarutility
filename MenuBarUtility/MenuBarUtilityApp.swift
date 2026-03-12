05b0c393ed64fd06dbf18bb846578c672151b7e8
Juan Urdaneta <jurdanetac10@gmail.com>
2026-03-12 14:17:12 -0400

    Initial sketch

--- MenuBarUtility/MenuBarUtilityApp.swift
+++ MenuBarUtility/MenuBarUtilityApp.swift
@@ -6,12 +6,52 @@
 //
 
 import SwiftUI
+import AppKit
 
 @main
 struct MenuBarUtilityApp: App {
+    // We use an AppDelegate to handle the system menu bar
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     var body: some Scene {
-        WindowGroup {
-            ContentView()
+        // You can leave this empty or hide it if you want a menu-bar-only app
+        Settings {
+            Text("Settings View")
         }
+    }
+}
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+    var statusItem: NSStatusItem?
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Create the status item in the menu bar
+        statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+        
+        if let button = statusItem?.button {
+            // Use an SF Symbol or a custom image
+            button.image = NSImage(systemSymbolName: "star.fill", accessibilityDescription: "App Icon")
+            button.action = #selector(toggleMenu)
+        }
+        
+        setupMenu()
+    }
+
+    func setupMenu() {
+        let menu = NSMenu()
+        
+        menu.addItem(NSMenuItem(title: "Hello World", action: #selector(doSomething), keyEquivalent: "h"))
+        menu.addItem(NSMenuItem.separator())
+        menu.addItem(NSMenuItem(title: "Quit", action: #selector(NSApplication.terminate(_:)), keyEquivalent: "q"))
+        
+        statusItem?.menu = menu
+    }
+
+    @objc func doSomething() {
+        print("Menu item clicked!")
+    }
+    
+    @objc func toggleMenu() {
+        // This is where you would trigger a Popover or Menu
     }
 }
