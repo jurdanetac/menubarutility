1a7faf4af76a9e7ae2b38c885a4ba9f206277167
Juan Urdaneta <jurdanetac10@gmail.com>
2026-03-12 13:53:02 -0400

    Initial Commit

---
+++ MenuBarUtility/MenuBarUtilityApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenuBarUtilityApp.swift
+//  MenuBarUtility
+//
+//  Created by Top Of The Line VA on 3/12/26.
+//
+
+import SwiftUI
+
+@main
+struct MenuBarUtilityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
